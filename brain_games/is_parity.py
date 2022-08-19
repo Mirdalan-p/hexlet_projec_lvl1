1151fd4b8cc03af507cf420025a3293c2f0b0f66
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-19 20:50:31 +0300

    изменения в логике

--- brain_games/is_parity.py
+++ brain_games/is_parity.py
@@ -2,23 +2,30 @@ from calendar import c
 import prompt
 from random import randint
 
+
+print("brain-games\nWelcome to the Brain Games!")
+name = prompt.string('May I have your name? ')
+print(f"Hello, {name}!")
+random_num = randint(1, 100)
+
 def parity_check():
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    random_num = randint(1, 100)
     correct_answ_count = 0 
-    answer_is = ' '
     while correct_answ_count < 3:
         print(f'Question: {random_num}')
         answer_is = prompt.string('Your answer: ')
-        if answer_is == "Yes" and random_num == 0:
+        if answer_is == "Yes" and random_num % 2 == 0:
             print('Correct')
             correct_answ_count += 1
-        elif answer_is == 'No' and random_num != 0:
+        elif answer_is == 'No' and random_num % 2 != 0:
             print('Correct')
             correct_answ_count += 1
         else:
-            print('Your answer is wrong')
-            correct_answ_count == 0
-    print('Congratulations')
+            if answer_is == 'Yes':
+                print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again,' + name + '!'")
+                parity_check()
+    print(f"Congratulations, {name}!")
+
+
         
          
