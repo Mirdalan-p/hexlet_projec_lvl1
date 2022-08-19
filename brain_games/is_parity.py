7d02e55db867c98e8f9f4dfb13180c19b0b17fea
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-19 21:08:37 +0300

    логика заработала

--- brain_games/is_parity.py
+++ brain_games/is_parity.py
@@ -6,26 +6,29 @@ from random import randint
 print("brain-games\nWelcome to the Brain Games!")
 name = prompt.string('May I have your name? ')
 print(f"Hello, {name}!")
-random_num = randint(1, 100)
+
 
 def parity_check():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     correct_answ_count = 0 
     while correct_answ_count < 3:
+        random_num = randint(1, 100)
         print(f'Question: {random_num}')
         answer_is = prompt.string('Your answer: ')
-        if answer_is == "Yes" and random_num % 2 == 0:
+        if answer_is == "yes" and random_num % 2 == 0:
             print('Correct')
             correct_answ_count += 1
-        elif answer_is == 'No' and random_num % 2 != 0:
+        elif answer_is == 'no' and random_num % 2 != 0:
             print('Correct')
             correct_answ_count += 1
         else:
-            if answer_is == 'Yes':
-                print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again,' + name + '!'")
-                parity_check()
+            if answer_is == 'yes' and random_num % 2 != 0:
+                print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
+            elif answer_is == 'no' and random_num %2 == 0:
+                print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
+            correct_answ_count = 0
     print(f"Congratulations, {name}!")
 
-
-        
+parity_check()
+      
          
