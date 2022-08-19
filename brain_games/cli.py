d3549c4de15affefe071c6454390270895d55948
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-19 11:26:22 +0300

    fix mistakes

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,7 @@
+import prompt
+
+
 def welcome_user():
-    import prompt
-
-
     print("brain-games\nWelcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
-welcome_user()
