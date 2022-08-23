dd9534d45adf4b0b713f8dbe80e19ddb5f6a2f58
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-23 22:29:16 +0300

    пофиксил task

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,6 +7,6 @@ question = "Find the greatest common divisor of given numbers."
 def game_logic():
     num_1 = randint(1, 100)
     num_2 = randint(1, 100)
-    task = f"{num_1}, {num_2}"
+    task = (f"{num_1} {num_2}")
     result = math.gcd(num_1, num_2)
     return task, str(result)
