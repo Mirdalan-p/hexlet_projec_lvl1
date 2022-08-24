f39caca45681291f219d6509e1d7daaeafdee6f7
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-24 14:43:23 +0300

    переименовал

---
+++ brain_games/games/even.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+GAME_QUESTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def game_logic():
+    random_num = randint(1, 100)
+    task = random_num
+    if random_num % 2 == 0:
+        result = 'yes'
+    else:
+        result = 'no'
+    return task, result
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games import is_parity
+from brain_games.games import even
 from brain_games.engine import run
 
 
 def main():
     print("Welcome to the Brain Games!")
-    run(is_parity)
+    run(even)
 
 
 if __name__ == '__main__':
