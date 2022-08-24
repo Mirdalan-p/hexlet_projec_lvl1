40b61889f6b3ce8ee6ec805da3b6d7eae10e42a1
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-24 16:57:43 +0300

    теперь логика определяет 0, 1 как не простые числа

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,10 +6,9 @@ GAME_QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def generate_round():
     random_num = randint(0, 100)
     task = f"{random_num}"
-
-    def prime_check():
+    def prime_check(num):
         i = 0
-        for devider in range(2, (random_num // 2 + 1)):
+        for devider in range(2, (num // 2 + 1)):
             if random_num % devider == 0:
                 i += 1
         if i == 0:
@@ -17,5 +16,8 @@ def generate_round():
         else:
             result = 'no'
         return result
-    result = prime_check()
+    if random_num < 2:
+        result = 'no'
+    else:
+        result = prime_check(random_num)
     return task, result
