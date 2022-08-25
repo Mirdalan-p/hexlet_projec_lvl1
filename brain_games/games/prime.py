acc1fbf1fa97856d37678db4e618b3f913caf03e
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-25 18:07:02 +0300

    вынес логику вычислений в отдельную функцию

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,22 +3,22 @@ from random import randint
 GAME_QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def generate_round():
-    random_num = randint(0, 100)
-    task = f"{random_num}"
-
-    def prime_check(num):
+def is_prime(num):
         i = 0
         for devider in range(2, (num // 2 + 1)):
-            if random_num % devider == 0:
+            if num % devider == 0:
                 i += 1
-        if i == 0:
+        if num < 2:
+            result = 'no'
+        elif i == 0:
             result = 'yes'
         else:
             result = 'no'
         return result
-    if random_num < 2:
-        result = 'no'
-    else:
-        result = prime_check(random_num)
+
+
+def generate_round():
+    random_num = randint(0, 100)
+    task = f"{random_num}"
+    result = is_prime(random_num)
     return task, result
