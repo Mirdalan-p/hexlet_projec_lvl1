3b1c5e34bc2b30ca94b76d96667d6ba7a7f4e2e5
Ivan <mirdalan@WIN-V21LRHOVH2E.localdomain>
2022-08-25 18:16:24 +0300

    вынес вычисления в отдельную функцию

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,15 +4,20 @@ from random import randint
 GAME_QUESTION = "Find the greatest common divisor of given numbers."
 
 
-def generate_round():
-    num_1 = randint(1, 100)
-    num_2 = randint(1, 100)
-    task = (f"{num_1} {num_2}")
-
+def gcd(num_1, num_2):
     while num_1 != 0 and num_2 != 0:
         if num_1 > num_2:
             num_1 = num_1 % num_2
         else:
             num_2 = num_2 % num_1
     result = num_1 + num_2
-    return task, str(result)
+    return str(result)
+
+
+def generate_round():
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    task = (f"{num_1} {num_2}")
+    result = gcd(num_1, num_2)
+    
+    return task, result
